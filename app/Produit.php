577d619ef2ab491e62e45ddf47d956aec702ea12
arwasallemi <37615270+arwasallemi@users.noreply.github.com>
2019-07-12 08:18:01 +0100

    Update Produit.php

--- app/Produit.php
+++ app/Produit.php
@@ -14,11 +14,8 @@ class Produit extends Model
      */
     protected $table='produit';
     protected $fillable = [
-<<<<<<< HEAD
-        'libelle', 'ref', 'note', 'prix_location','prix_location_minimale', 'qrCode', 'codeBarre','image','categorie'
-=======
         'libelle', 'ref','categorie', 'note', 'prix_location','prix_location_minimale', 'qrCode', 'codeBarre','image'
->>>>>>> 3f5331b6183801143a7228e35aac859c739832ef
+
     ];
 
     /**
